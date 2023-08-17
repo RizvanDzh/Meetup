c8d0ff949c7b97fc680ecacabc382ff2f1db5732
Rizvan Dzhamaludinov <rdzhamaludinov@fil-it.ru>
2023-08-17 19:06:33 +0300

    сделал jwt Токен

--- meetup/src/App.js
+++ meetup/src/App.js
@@ -1,14 +1,19 @@
-import Header from './shared/header/Header';
 import Auth from './pages/auth/Auth';
+import Meetup from './pages/meetup/Meetup';
+import Header from './shared/header/Header';
 import Footer from './shared/footer/Footer';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.scss';
+
+const token = JSON.parse(localStorage.getItem('user'));
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Auth />
+      <BrowserRouter>
+        <Routes>{token ? <Route path="/meetup" element={<Meetup />} /> : <Route path="/" element={<Auth />} />}</Routes>
+      </BrowserRouter>
       <Footer />
     </div>
   );
